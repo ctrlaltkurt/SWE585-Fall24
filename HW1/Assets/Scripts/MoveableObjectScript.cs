3f1e2e27378f72c9bcf7de1b68351953b12641a0
Onur Altınkurt <onuraltinkurt@nimportequoi.local>
2024-10-20 11:15:47 +0300

    2.6 Applying forces to RigidBodies

--- HW1/Assets/Scripts/MoveableObjectScript.cs
+++ HW1/Assets/Scripts/MoveableObjectScript.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class MoveableObjectScript : MonoBehaviour
 {
+    // Variable to hold a reference to the object's Rigidbody
+    public Rigidbody rb;
+
+    // Force amount
+    public Vector3 force = new Vector3(0, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If rb hasn't been set in the Inspector, find it on this GameObject
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // Move the object up when the 'W' key is pressed
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * 5);
-        }
+        // Apply force to the Rigidbody every frame
+        rb.AddForce(force);
     }
 }
 
